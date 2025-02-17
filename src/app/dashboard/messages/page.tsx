073ee856c49f99c15df7e1cc942f67d31bdbe882
shahidulllah <mdshahidsumon177@gmail.com>
2025-02-17 21:40:05 +0600

    dashboard messages management

---
+++ src/app/dashboard/messages/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { IContact } from "@/types/contact.type";
+import { useEffect, useState } from "react";
+
+export default function MessageManagement() {
+  const [messages, setMessages] = useState<IContact[]>([]);
+
+  useEffect(() => {
+    async function fetchMessages() {
+      const res = await fetch("/api/contact");
+      const data = await res.json();
+      setMessages(data);
+    }
+    fetchMessages();
+  }, []);
+
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Messages</h1>
+      <div className="space-y-4">
+        {messages.map((msg) => (
+          <div key={msg._id} className="bg-white p-4 shadow rounded">
+            <h2 className="text-xl font-semibold">{msg.name}</h2>
+            <p className="text-gray-600">{msg.email}</p>
+            <p className="text-gray-600">{msg.message}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/types/contact.type.ts
+++ src/types/contact.type.ts
@@ -1,6 +1,7 @@
 import { Document } from "mongoose";
 
 export interface IContact extends Document {
+    _id: string
     name: string;
     email: string;
     message: string;

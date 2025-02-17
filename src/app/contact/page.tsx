9e0faf186fbd9f661dbb2a722ddf7f5fa1f98c6f
shahidulllah <mdshahidsumon177@gmail.com>
2025-02-17 21:18:48 +0600

    form functionality almost done

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -7,33 +7,28 @@ import Link from "next/link";
 import Image from "next/image";
 
 const EmailSection = () => {
-  const [emailSubmitted, setEmailSubmitted] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
 
-  const handleSubmit = async (e) => {
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    const data = {
-      email: e.target.email.value,
-      subject: e.target.subject.value,
-      message: e.target.message.value,
-    };
-    const JSONdata = JSON.stringify(data);
-    const endpoint = "/api/send";
-
-    const options = {
+    const res = await fetch("/api/contact", {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSONdata,
-    };
-
-    const response = await fetch(endpoint, options);
-    const resData = await response.json();
-    console.log(resData);
-
-    if (response.status === 200) {
-      console.log("Message sent.");
-      setEmailSubmitted(true);
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formData),
+    });
+    if (res.ok) {
+      alert("Message sent successfully!");
+      setFormData({ name: "", email: "", message: "" });
     }
   };
 
@@ -49,7 +44,6 @@ const EmailSection = () => {
             Let&apos;s Connect
           </h5>
           <p className="text-[#ADB7BE] mb-4 max-w-md">
-            {" "}
             I&apos;m currently looking for new opportunities, my inbox is always
             open. Whether you have a question or just want to say hi, I&apos;ll
             try my best to get back to you!
@@ -64,66 +58,63 @@ const EmailSection = () => {
           </div>
         </div>
         <div className="mt-6">
-          {emailSubmitted ? (
-            <p className="text-green-500 text-sm mt-2">
-              Email sent successfully!
-            </p>
-          ) : (
-            <form className="flex flex-col" onSubmit={handleSubmit}>
-              <div className="mb-6">
-                <label
-                  htmlFor="email"
-                  className="text-white block mb-2 text-sm font-medium"
-                >
-                  Your email
-                </label>
-                <input
-                  name="email"
-                  type="email"
-                  id="email"
-                  required
-                  className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                  placeholder="jacob@google.com"
-                />
-              </div>
-              <div className="mb-6">
-                <label
-                  htmlFor="subject"
-                  className="text-white block text-sm mb-2 font-medium"
-                >
-                  Subject
-                </label>
-                <input
-                  name="subject"
-                  type="text"
-                  id="subject"
-                  required
-                  className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                  placeholder="Just saying hi"
-                />
-              </div>
-              <div className="mb-6">
-                <label
-                  htmlFor="message"
-                  className="text-white block text-sm mb-2 font-medium"
-                >
-                  Message
-                </label>
-                <textarea
-                  name="message"
-                  id="message"
-                  className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                  placeholder="Let's talk about..."
-                />
-              </div>
-              <button
-                type="submit"
-                className="bg-purple-500 hover:bg-purple-600 text-white font-medium py-2.5 px-5 rounded-lg w-full"
+          <form className="flex flex-col" onSubmit={handleSubmit}>
+            <div className="mb-6">
+              <label
+                htmlFor="name"
+                className="text-white block mb-2 text-sm font-medium"
               >
-                Send Message
-              </button>
-            </form>
-          )}
+                Name
+              </label>
+              <input
+                onChange={handleChange}
+                name="name"
+                type="text"
+                id="name"
+                required
+                className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                placeholder="Place your name"
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                htmlFor="email"
+                className="text-white block text-sm mb-2 font-medium"
+              >
+                Your email
+              </label>
+              <input
+                onChange={handleChange}
+                name="email"
+                type="email"
+                id="email"
+                required
+                className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                placeholder="Give your E-mail"
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                htmlFor="message"
+                className="text-white block text-sm mb-2 font-medium"
+              >
+                Message
+              </label>
+              <textarea
+                onChange={handleChange}
+                name="message"
+                id="message"
+                className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+                placeholder="Let's talk about..."
+              />
+            </div>
+            <button
+              type="submit"
+              className="bg-purple-500 hover:bg-purple-600 text-white font-medium py-2.5 px-5 rounded-lg w-full"
+            >
+              Send Message
+            </button>
+          </form>
         </div>
       </div>
     </section>

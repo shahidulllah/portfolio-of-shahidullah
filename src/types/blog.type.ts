4bc319a824b265763f54a0774e1f09723f27a305
shahidulllah <mdshahidsumon177@gmail.com>
2025-04-08 23:37:54 +0600

    blog type

--- src/types/blog.type.ts
+++ src/types/blog.type.ts
@@ -1,9 +1,9 @@
 import { Document } from "mongoose";
 
 export interface IBlog extends Document {
-    _id: string;
-    title: string;
-    content: string;
-    image?: string;
-    category: string;
-  }
+  _id: string;
+  title: string;
+  content: string;
+  image?: string;
+  category: string;
+}

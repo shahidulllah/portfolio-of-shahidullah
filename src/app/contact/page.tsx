e95354512c8687a4dad729aaa30ee73db550d4c4
shahidulllah <mdshahidsumon177@gmail.com>
2025-05-08 01:31:33 +0600

    contact page modify

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -42,12 +42,11 @@ const EmailSection = () => {
         id="contact"
         className="grid md:grid-cols-2 my-12 md:my-12 py-12 lg:py-20 gap-4 relative p-4 max-w-7xl mx-auto"
       >
-        <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2 p-4 lg:mx-24"></div>
         <div className="z-9 my flex flex-col justify-center text-center lg:text-left">
-          <h5 className="text-3xl font-bold text-white my-2">
+          <h5 className="text-3xl font-bold text-black dark:text-white my-2">
             Let&apos;s Connect
           </h5>
-          <p className="text-[#ADB7BE] mb-4 max-w-md">
+          <p className="text-black dark:text-white mb-4 max-w-md">
             I&apos;m currently looking for new opportunities, my inbox is always
             open. Whether you have a question or just want to say hi, I&apos;ll
             try my best to get back to you!
@@ -66,7 +65,7 @@ const EmailSection = () => {
             <div className="mb-6">
               <label
                 htmlFor="name"
-                className="text-white block mb-2 text-sm font-medium"
+                className="text-black dark:text-white block mb-2 text-sm font-medium"
               >
                 Name
               </label>
@@ -83,7 +82,7 @@ const EmailSection = () => {
             <div className="mb-6">
               <label
                 htmlFor="email"
-                className="text-white block text-sm mb-2 font-medium"
+                className="text-black dark:text-white block text-sm mb-2 font-medium"
               >
                 Your email
               </label>
@@ -100,7 +99,7 @@ const EmailSection = () => {
             <div className="mb-6">
               <label
                 htmlFor="message"
-                className="text-white block text-sm mb-2 font-medium"
+                className="text-black dark:text-white block text-sm mb-2 font-medium"
               >
                 Message
               </label>

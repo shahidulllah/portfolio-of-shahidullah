c31f0f401ab3d0a4987170303d1cf0c4fe143fdb
shahidulllah <mdshahidsumon177@gmail.com>
2025-05-31 09:04:21 +0600

    project model modified

--- src/models/Project.ts
+++ src/models/Project.ts
@@ -1,13 +1,23 @@
 import { IProject } from "@/types/project.type";
 import mongoose, { Schema } from "mongoose";
 
-const ProjectSchema = new Schema<IProject>({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  image: { type: String, required: true },
-  liveUrl: { type: String, required: true },
-  githubUrl: { type: String, required: true },
-});
+const ProjectSchema = new Schema<IProject>(
+  {
+    title: { type: String, required: true },
+    image: { type: String, required: true },
+    category: { type: String, default: "General" },
+    description: { type: String, required: true },
+    technologies: [{ type: String, required: true }],
+    goals: { type: String, required: true },
+    challenges: [{ type: String, required: true }],
+    learnings: { type: String, required: true },
+    liveUrl: { type: String, required: true },
+    githubUrl: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 export default mongoose.models.Project ||
   mongoose.model<IProject>("Project", ProjectSchema);

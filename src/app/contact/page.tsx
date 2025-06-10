e3cb5acd3002e3e34f748a5bcfb3547fcf126d8e
shahidulllah <mdshahidsumon177@gmail.com>
2025-06-10 09:12:09 +0600

    trying to fixed email sending error

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -29,8 +29,6 @@ const EmailSection = () => {
     });
     if (res.ok) {
       toast.success("Message sent successfully!");
-
-      setFormData({ name: "", email: "", message: "" });
     } else {
       toast.error("Failed to send message. Please try again.");
     }

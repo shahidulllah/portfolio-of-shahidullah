b45d9758644aa1eabf7ca3020ea967700bb758b9
shahidulllah <mdshahidsumon177@gmail.com>
2025-06-12 20:13:52 +0600

    conmment delete

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -16,7 +16,7 @@ export async function POST(req: Request) {
     const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
-        user: process.env.SMTP_USER, // your Gmail address
+        user: process.env.SMTP_USER, 
         pass: process.env.SMTP_PASS, // App Password (not your Gmail password!)
       },
     });

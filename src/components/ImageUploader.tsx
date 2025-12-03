72f7cf95085f2723e35c0f270e740552508aab2a
shahidulllah <mdshahidsumon177@gmail.com>
2025-12-03 21:03:55 +0600

    image uploader component

---
+++ src/components/ImageUploader.tsx
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+
+export default function ImageUploader() {
+  const [imageUrl, setImageUrl] = useState("");
+
+  const handleUpload = async (e: any) => {
+    const file = e.target.files[0];
+    if (!file) return;
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    const res = await fetch("/api/upload", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await res.json();
+    setImageUrl(data.url);
+  };
+
+  return (
+    <div>
+      <input
+        type="file"
+        accept="image/*"
+        onChange={handleUpload}
+        className="border p-2"
+      />
+
+      {imageUrl && (
+        <Image src={imageUrl} alt="Uploaded" className="mt-4 w-40 rounded" />
+      )}
+    </div>
+  );
+}

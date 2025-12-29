731146b788979cedce6f02d0ea2b69792e6449b1
shahidulllah <mdshahidsumon177@gmail.com>
2025-12-30 00:06:03 +0600

    code refectoring

--- src/app/api/watch/categories/route.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { categories } from "@/lib/watchData";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  return NextResponse.json(categories.map(({ playlists, ...rest }) => rest));
-}
--- src/app/api/watch/playlists/[category]/route.ts
+++ src/app/api/watch/playlists/[category]/route.ts
@@ -1,12 +1,12 @@
-import { categories } from "@/lib/watchData";
-import { NextResponse } from "next/server";
+// import { categories } from "@/lib/watchData";
+// import { NextResponse } from "next/server";
 
-export async function GET(
-  req: Request,
-  { params }: { params: { category: string } }
-) {
-  const category = categories.find((c) => c.slug === params.category);
-  if (!category) return NextResponse.json([], { status: 404 });
+// export async function GET(
+//   req: Request,
+//   { params }: { params: { category: string } }
+// ) {
+//   const category = categories.find((c) => c.slug === params.category);
+//   if (!category) return NextResponse.json([], { status: 404 });
 
-  return NextResponse.json(category.playlists);
-}
+//   return NextResponse.json(category.playlists);
+// }
